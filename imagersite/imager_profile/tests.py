b0ecc14ee946cd7f14cc2f4a6e73c7b3aef8938a
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-23 16:06:26 -0700

    added the first of the factory.Faker

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import ImagerProfile, User
+import factory
 
 # Create your tests here.
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = ImagerProfile
+
+    bio = factory.Faker('')
+    phone = factory.Faker('')
+    location = factory.Faker('')
+    website = factory.Faker('')
+    fee = factory.Faker('')
+    camera = factory.Faker('')
+    services = factory.Faker('')
+    photostyles = factory.Faker('')
