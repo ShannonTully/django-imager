86370a188ace78a0ba4c76d78acba1d5b81aaeac
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-25 14:51:22 -0700

    added Pillow, req.txt

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from sorl.thumbnail import ImageField
+
+
 # Create your models here.
-class 
+class Photo(models.Model):
+    """this the the Photo model"""
+    album = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='photos')
+    title = models.CharField(max_length=180, default="Untitled")
+    description = models.TextField(blank=True, null=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    image = ImageField(upload_to="images")
+    published = models.CharField(max_length=7, choices=(('PRIVATE', 'Private'), ('SHARED', 'Shared'), ('PUBLIC', 'Public')))
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Album(models.Model):
+    """this is the model Album model"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='albums')
+    cover = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+    title = models.CharField(max_length=180, default="Untitled")
+    description = models.TextField(blank=True, null=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    published = models.CharField(max_length=7, choices=(('PRIVATE', 'Private'), ('SHARED', 'Shared'), ('PUBLIC', 'Public')))
+
+    def __str__(self):
+        return '{}'.format(self.title)
--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'imagersite',
     'imager_profile',
+    'imager_images'
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ACCOUNT_ACTIVATION_DAYS = 1
 LOGIN_REDIRECT_URL = '/'
