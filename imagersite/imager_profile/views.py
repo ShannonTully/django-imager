645ba8f72a44cd4295b43c0602e1a4b5bd9b4d41
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-26 15:04:36 -0700

    added the profile_view.py code.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,30 +1,32 @@
-from django.shortcuts import render, redirect
+"""Make the profile view page."""
+
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ImagerProfile
 from imager_images.models import Album, Photo
 
 
-# Create your views here.
 def profile_view(request, username=None):
-    """Doc string."""
+    """Define the profive view."""
     owner = False
 
     if not username:
         username = request.user.get_username()
+        owner = True
         if username == '':
             return redirect('home')
 
     profile = get_object_or_404(ImagerProfile, user__username=username)
     albums = Album.objects.filter(user__username=username)
-    photos = Photo.objects.filter(albums__user__username=usermane)
+    photos = Photo.objects.filter(album__user__username=username)
 
     if not owner:
         photos = Photo.objects.filter(published='PUBLIC')
         albums = Album.objects.filter(published='PUBLIC')
 
     context = {
-        'profile': profile'
+        'profile': profile,
         'albums': albums,
         'photos': photos
     }
 
-    return render(request, 'profile.html', context)
+    return render(request, 'imager_profile/profile.html', context)
