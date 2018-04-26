b9ba8fa1e5de18cb9cc0905b17dce6ae91133820
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-26 15:39:26 -0700

    added the logic for the profile view

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -6,7 +6,7 @@ from imager_images.models import Album, Photo
 
 
 def profile_view(request, username=None):
-    """Define the profive view."""
+    """Define the profile view."""
     owner = False
 
     if not username:
@@ -18,15 +18,28 @@ def profile_view(request, username=None):
     profile = get_object_or_404(ImagerProfile, user__username=username)
     albums = Album.objects.filter(user__username=username)
     photos = Photo.objects.filter(album__user__username=username)
+    public_photos = photos.filter(published='PUBLIC')
+    public_albums = albums.filter(published='PUBLIC')    
 
     if not owner:
         photos = Photo.objects.filter(published='PUBLIC')
         albums = Album.objects.filter(published='PUBLIC')
 
+    num_albums = len(albums)
+    num_photos = len(photos)
+    num_public_albums = len(public_albums)
+    num_public_photos = len(public_photos)
+
     context = {
         'profile': profile,
         'albums': albums,
-        'photos': photos
+        'photos': photos,
+        'num_albums': num_albums,
+        'num_photos': num_photos,
+        'num_public_albums': num_public_albums,
+        'num_public_photos': num_public_photos,
     }
 
     return render(request, 'imager_profile/profile.html', context)
+
+
