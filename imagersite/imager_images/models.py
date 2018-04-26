925a9d84adcad420664d57ca7b59484c86dd6862
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-26 09:56:28 -0700

    dos strings

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,11 +1,15 @@
+"""Make the models for the Photos and Albums."""
+
 from django.db import models
 from django.contrib.auth.models import User
 from sorl.thumbnail import ImageField
 
 
 # Create your models here.
+
 class Photo(models.Model):
-    """this the the Photo model"""
+    """Make the Photo model."""
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
     albums = models.ManyToManyField('Album', related_name='photos')
     title = models.CharField(max_length=180, default="Untitled")
@@ -14,14 +18,21 @@ class Photo(models.Model):
     date_modified = models.DateField(auto_now=True)
     date_published = models.DateField(blank=True, null=True)
     image = ImageField(upload_to="images")
-    published = models.CharField(max_length=7, choices=(('PRIVATE', 'Private'), ('SHARED', 'Shared'), ('PUBLIC', 'Public')))
+    published = models.CharField(
+        max_length=7,
+        choices=(
+            ('PRIVATE', 'Private'),
+            ('SHARED', 'Shared'),
+            ('PUBLIC', 'Public'))
+        )
 
     def __str__(self):
+        """Show a string representation of the title."""
         return '{}'.format(self.title)
 
 
 class Album(models.Model):
-    """this is the model Album model"""
+    """Make the Album model."""
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='albums')
     cover = models.ForeignKey(Photo, on_delete=models.SET_NULL, related_name='+', null=True, blank=True)
     title = models.CharField(max_length=180, default="Untitled")
@@ -29,7 +40,14 @@ class Album(models.Model):
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
     date_published = models.DateField(blank=True, null=True)
-    published = models.CharField(max_length=7, choices=(('PRIVATE', 'Private'), ('SHARED', 'Shared'), ('PUBLIC', 'Public')))
+    published = models.CharField(
+        max_length=7,
+        choices=(
+            ('PRIVATE', 'Private'),
+            ('SHARED', 'Shared'),
+            ('PUBLIC', 'Public'))
+        )
 
     def __str__(self):
+        """Show a string representation of the title."""
         return '{}'.format(self.title)
