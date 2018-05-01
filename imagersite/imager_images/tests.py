2a6a987af1b54337b93e383045247d2f74c4b4b4
Shannon Tully <scatman.st@gmail.com>
2018-04-30 17:20:54 -0700

    tests

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -2,7 +2,9 @@
 
 from django.test import TestCase
 from .models import Photo, Album, User
+from django.urls import reverse_lazy
 import factory
+import tempfile
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -42,6 +44,7 @@ class PhotoFactory(factory.django.DjangoModelFactory):
     title = factory.Faker('company')
     date_uploaded = factory.Faker('date_time')
     date_modified = factory.Faker('date_time')
+    image = tempfile.NamedTemporaryFile(suffix=".jpg").name
     published = 'PUBLIC'
 
 
@@ -125,3 +128,125 @@ class PhotoUnitTests(TestCase):
         """Test the photo."""
         one_album = Album.objects.first()
         self.assertEqual(one_album.photos.first().description, one_album.photos.first().description)
+
+
+class ImageViewTests(TestCase):
+    """Unit tests for the image views."""
+
+    @classmethod
+    def setUp(self):
+        """Test setup."""
+        super(TestCase, self)
+        # fake = Faker()
+        for _ in range(5):
+            self.user = UserFactory.create()
+            self.user.set_password(factory.Faker('password'))
+            self.user.save()
+
+            self.album = AlbumFactory.create(user=self.user)
+            self.album.save()
+
+            self.photo = PhotoFactory.create(user=self.user)
+            self.photo.save()
+
+            # photo.albums.add(album)
+            self.album.photos.add(self.photo)
+
+    @classmethod
+    def tearDown(self):
+        """Tear down for test."""
+        super(TestCase, self)
+        User.objects.all().delete()
+        Photo.objects.all().delete()
+
+    def test_image_view_library_profile(self):
+        """Test image view library profile."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('library'))
+        self.assertEqual(response.context['profile'].username, self.user.username)
+
+    def test_image_view_library_photos(self):
+        """Test image view library photos."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('library'))
+        self.assertEqual(len(response.context['photos']), 1)
+
+    def test_image_view_library_albums(self):
+        """Test image view library albums."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('library'))
+        self.assertEqual(len(response.context['albums']), 1)
+
+    def test_image_view_public_albums(self):
+        """Test image view public_albums."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('album'))
+        self.assertEqual(len(response.context['public_albums']), 5)
+
+    def test_image_view_public_albums_photo(self):
+        """Test image view photo on a public album."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('album'))
+        self.assertIsNotNone(response.context['public_albums'].first().photos)
+
+    def test_image_view_albums_public(self):
+        """Test image view albums are public."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('album'))
+        self.assertEqual(response.context['public_albums'].first().published, 'PUBLIC')
+
+    def test_image_view_public_photos(self):
+        """Test image view public_photos."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('photo'))
+        self.assertEqual(len(response.context['public_photos']), 5)
+
+    def test_image_view_public_photos_album(self):
+        """Test image view album on a public photo."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('photo'))
+        self.assertIsNotNone(response.context['public_photos'].first().albums)
+
+    def test_image_view_photos_public(self):
+        """Test image view photos are public."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('photo'))
+        self.assertEqual(response.context['public_photos'].first().published, 'PUBLIC')
+
+    def test_image_view_album_detail(self):
+        """Test image view album detail."""
+        self.client.force_login(self.user)
+        album = Album.objects.first()
+        response = self.client.get(reverse_lazy('album_detail', kwargs={'id': album.id}))
+        self.assertEqual(response.context['this_album'].published, 'PUBLIC')
+
+    def test_image_view_album_detail_invalid(self):
+        """Test image view album detail that doesnt exist."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('album_detail', kwargs={'id': 9999}))
+        self.assertEqual(response.context['this_album'], None)
+
+    def test_image_view_album_detail_not_signed_in(self):
+        """Test image view album detail while not signed in."""
+        album = Album.objects.first()
+        response = self.client.get(reverse_lazy('album_detail', kwargs={'id': album.id}))
+        self.assertEqual(response.context['this_album'].published, 'PUBLIC')
+
+    def test_image_view_photo_detail(self):
+        """Test image view photo detail."""
+        self.client.force_login(self.user)
+        photo = Photo.objects.first()
+        response = self.client.get(reverse_lazy('photo_detail', kwargs={'id': photo.id}))
+        self.assertIsNotNone(response.context['this_photo'])
+
+    def test_image_view_photo_detail_invalid(self):
+        """Test image view photo detail that doesnt exist."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('photo_detail', kwargs={'id': 9999}))
+        self.assertEqual(response.context['this_photo'], None)
+
+    def test_image_view_photo_detail_not_signed_in(self):
+        """Test image view photo detail while not signed in."""
+        photo = Photo.objects.first()
+        response = self.client.get(reverse_lazy('photo_detail', kwargs={'id': photo.id}))
+        self.assertIsNotNone(response.context['this_photo'])
