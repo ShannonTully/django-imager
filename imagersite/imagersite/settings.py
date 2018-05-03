73981f40ff6aebc5c129cc7dd44289836b814c2b
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-03 16:33:33 -0700

    updated the settings.py and .env for email

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -137,3 +137,10 @@ ACCOUNT_ACTIVATION_DAYS = 1
 LOGIN_REDIRECT_URL = '/'
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
+EMAIL_PORT = os.environ.get('EMAIL_PORT', None)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', None)
+EMAIL_TIMEOUT = os.environ.get('EMAIL_TIMEOUT', None)
