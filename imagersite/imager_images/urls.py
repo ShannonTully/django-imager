bd660bfbb666c2d21e732b142268b59972e26652
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-08 16:57:32 -0700

    finished the library view

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,13 +1,13 @@
 """Page doc string."""
 
 from django.urls import path
-from .views import library_view, album_view, photo_view, album_detail_view, photo_detail_view
+from .views import LibraryView  # , album_view, photo_view, album_detail_view, photo_detail_view
 
 
 urlpatterns = [
-    path('library/', library_view, name='library'),
-    path('albums/', album_view, name='album'),
-    path('photos/', photo_view, name='photo'),
-    path('albums/<int:id>', album_detail_view, name='album_detail'),
-    path('photos/<int:id>', photo_detail_view, name='photo_detail'),
+    path('library/', LibraryView.as_view(), name='library'),
+    # path('albums/', album_view, name='album'),
+    # path('photos/', photo_view, name='photo'),
+    # path('albums/<int:id>', album_detail_view, name='album_detail'),
+    # path('photos/<int:id>', photo_detail_view, name='photo_detail'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,68 +1,114 @@
 """Views for imager_images."""
 
+from django.views.generic.list import ListView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Album, Photo
 from django.contrib.auth.models import User
 
 
-def library_view(request):
-    """Define the library view."""
-    username = request.user.get_username()
+class LibraryView(ListView):
+    """Define the library view class."""
 
-    if username == '':
-        return redirect('home')
-    profile = get_object_or_404(User, username=username)
-    albums = Album.objects.filter(user__username=username)
-    photos = Photo.objects.filter(user__username=username)
+    template_name = 'imager_images/library.html'
+    context_object_name = 'albums_and_photos'
 
-    context = {
-        'profile': profile,
-        'albums': albums,
-        'photos': photos,
-    }
+    def get(self, *args, **kwargs):
+        """Check that user is authenticated, get args and kwargs."""
+        if not self.request.user.is_authenticated:
+            return redirect('home')
 
-    return render(request, 'imager_images/library.html', context)
+        return super().get(*args, **kwargs)
 
+    def get_queryset(self):
+        """Get the context to display."""
+        username = self.request.user.get_username()
+        albums = Album.objects.filter(user__username=username)
+        photos = Photo.objects.filter(user__username=username)
 
-def album_view(request):
-    """Define the library view."""
-    public_albums = Album.objects.filter(published='PUBLIC')
+        return albums, photos     
 
-    context = {
-        'public_albums': public_albums,
-    }
+    def get_context_data(self, **kwargs):
+        """Filter the context for display."""
+        context = super().get_context_data(**kwargs)
+        albums = context['albums_and_photos'][0]
+        photos = context['albums_and_photos'][1]
 
-    return render(request, 'imager_images/album.html', context)
+        context.update({
+            'albums': albums,
+            'photos': photos,
+        })
 
-
-def photo_view(request):
-    """Define the library view."""
-    public_photos = Photo.objects.filter(published='PUBLIC')
-
-    context = {
-        'public_photos': public_photos,
-    }
-
-    return render(request, 'imager_images/photo.html', context)
+        return context
 
 
-def album_detail_view(request, id=None):
-    """Define the library view."""
-    this_album = Album.objects.filter(id=id).first()
-
-    context = {
-        'this_album': this_album,
-    }
-
-    return render(request, 'imager_images/album_detail.html', context)
 
 
-def photo_detail_view(request, id=None):
-    """Define the library view."""
-    this_photo = Photo.objects.filter(id=id).first()
 
-    context = {
-        'this_photo': this_photo,
-    }
 
-    return render(request, 'imager_images/photo_detail.html', context)
+
+
+
+
+
+
+# def library_view(request):
+#     """Define the library view."""
+#     username = request.user.get_username()
+
+#     if username == '':
+#         return redirect('home')
+#     profile = get_object_or_404(User, username=username)
+#     albums = Album.objects.filter(user__username=username)
+#     photos = Photo.objects.filter(user__username=username)
+
+#     context = {
+#         'profile': profile,
+#         'albums': albums,
+#         'photos': photos,
+#     }
+
+#     return render(request, 'imager_images/library.html', context)
+
+
+# def album_view(request):
+#     """Define the library view."""
+#     public_albums = Album.objects.filter(published='PUBLIC')
+
+#     context = {
+#         'public_albums': public_albums,
+#     }
+
+#     return render(request, 'imager_images/album.html', context)
+
+
+# def photo_view(request):
+#     """Define the library view."""
+#     public_photos = Photo.objects.filter(published='PUBLIC')
+
+#     context = {
+#         'public_photos': public_photos,
+#     }
+
+#     return render(request, 'imager_images/photo.html', context)
+
+
+# def album_detail_view(request, id=None):
+#     """Define the library view."""
+#     this_album = Album.objects.filter(id=id).first()
+
+#     context = {
+#         'this_album': this_album,
+#     }
+
+#     return render(request, 'imager_images/album_detail.html', context)
+
+
+# def photo_detail_view(request, id=None):
+#     """Define the library view."""
+#     this_photo = Photo.objects.filter(id=id).first()
+
+#     context = {
+#         'this_photo': this_photo,
+#     }
+
+#     return render(request, 'imager_images/photo_detail.html', context)
