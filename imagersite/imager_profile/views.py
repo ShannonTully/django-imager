5ba634ac79a1d15963fa943b3518a09be00cce63
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-08 15:28:56 -0700

    added the first half of the imager profile view

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,45 +1,103 @@
 """Make the profile view page."""
 
+from django.views.generic.list import DetailView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import ImagerProfile
 from imager_images.models import Album, Photo
 
 
-def profile_view(request, username=None):
-    """Define the profile view."""
-    owner = False
+class ProfileView(DetailView):
+    """Define the ProfileView class."""
 
-    if not username:
-        username = request.user.get_username()
-        owner = True
-        if username == '':
+    model = ImagerProfile
+    template_name = 'profile/'
+    context_object_name = 'profile'
+    slug_url_kwarg = 'username'
+    slug_field = 'user__username'
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
             return redirect('home')
 
-    profile = get_object_or_404(ImagerProfile, user__username=username)
-    albums = Album.objects.filter(user__username=username)
-    photos = Photo.objects.filter(albums__user__username=username)
-    public_photos = photos.filter(published='PUBLIC')
-    public_albums = albums.filter(published='PUBLIC')   
+        return super().get(*args, **kwargs)
 
-    if not owner:
-        photos = Photo.objects.filter(published='PUBLIC')
-        albums = Album.objects.filter(published='PUBLIC')
-
-    num_albums = len(albums)
-    num_photos = len(photos)
-    num_public_albums = len(public_albums)
-    num_public_photos = len(public_photos)
-
-    context = {
-        'profile': profile,
-        'albums': albums,
-        'photos': photos,
-        'num_albums': num_albums,
-        'num_photos': num_photos,
-        'num_public_albums': num_public_albums,
-        'num_public_photos': num_public_photos,
-    }
-
-    return render(request, 'imager_profile/profile.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = context['profile'].user.username
+        albums = Album.objects.filter(user__username=username)
+        photos = Photo.objects.filter(user__username=username)
+        public_photos = photos.filter(published='PUBLIC')
+        public_albums = albums.filter(published='PUBLIC')
+        num_albums = len(albums)
+        num_photos = len(photos)
+        num_public_albums = len(public_albums)
+        num_public_photos = len(public_photos)
+        context.update({
+            'albums': albums,
+            'photos': photos,
+            'num_albums': num_albums,
+            'num_photos': num_photos,
+            'num_public_albums': num_public_albums,
+            'num_public_photos': num_public_photos,
+        })
+        return context
 
 
+    # if not owner:
+    #     photos = Photo.objects.filter(published='PUBLIC')
+    #     albums = Album.objects.filter(published='PUBLIC')
+
+    # num_albums = len(albums)
+    # num_photos = len(photos)
+    # num_public_albums = len(public_albums)
+    # num_public_photos = len(public_photos)
+
+    # context = {
+    #     'profile': profile,
+    #     'albums': albums,
+    #     'photos': photos,
+    #     'num_albums': num_albums,
+    #     'num_photos': num_photos,
+    #     'num_public_albums': num_public_albums,
+    #     'num_public_photos': num_public_photos,
+    # }
+
+    # return render(request, 'imager_profile/profile.html', context)
+
+
+# def profile_view(request, username=None):
+#     """Define the profile view."""
+#     owner = False
+
+#     if not username:
+#         username = request.user.get_username()
+#         owner = True
+#         if username == '':
+#             return redirect('home')
+
+#     profile = get_object_or_404(ImagerProfile, user__username=username)
+#     albums = Album.objects.filter(user__username=username)
+#     photos = Photo.objects.filter(albums__user__username=username)
+#     public_photos = photos.filter(published='PUBLIC')
+#     public_albums = albums.filter(published='PUBLIC')   
+
+#     if not owner:
+#         photos = Photo.objects.filter(published='PUBLIC')
+#         albums = Album.objects.filter(published='PUBLIC')
+
+#     num_albums = len(albums)
+#     num_photos = len(photos)
+#     num_public_albums = len(public_albums)
+#     num_public_photos = len(public_photos)
+
+#     context = {
+#         'profile': profile,
+#         'albums': albums,
+#         'photos': photos,
+#         'num_albums': num_albums,
+#         'num_photos': num_photos,
+#         'num_public_albums': num_public_albums,
+#         'num_public_photos': num_public_photos,
+#     }
+
+#     return render(request, 'imager_profile/profile.html', context)
--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,15 +1,40 @@
-"""imagersite views."""
+"""Define the imagersite home view."""
 
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from imager_images.models import Photo
 
 
-def home_view(request):
-    """Get the home view."""
-    public_photos = Photo.objects.filter(published='PUBLIC')
+class HomeView(TemplateView):
+    """Make the HomeView class."""
 
-    context = {
-        'public_photos': public_photos,
-    }
+    template_name = 'generic/home.html'
 
-    return render(request, 'generic/home.html', context)
+    def get_context_photo(self, **kwargs):
+        """Get the context to fill the page."""
+        context = super().get_context_data(**kwargs)
+        public_photos = Photo.objects.filter(published='PUBLIC')
+
+        if public_photos.count():
+            image = public_photos.order_by('?').first()
+            image_url = image.image.url
+            image_title = image.title
+
+        else:
+            image_url = 'media/images/Honey_Hug.jpg'
+            image_title = 'HoneyHug'
+
+        context['image_url'] = image_url
+        context['image_title'] = image_title
+
+        return context
+
+
+# def home_view(request):
+#     """Get the home view."""
+#     public_photos = Photo.objects.filter(published='PUBLIC')
+
+#     context = {
+#         'public_photos': public_photos,
+#     }
+
+#     return render(request, 'generic/home.html', context)
